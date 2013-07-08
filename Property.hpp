c1d03e30f93d4adac07afb3944b4716bf59f4510
Alex <alex@deathstar.(none)>
2013-07-08 15:25:41 +0300

    Initial commit

---
+++ Property.hpp
@@ -0,0 +1,130 @@
+# pragma once
+
+template <typename T>
+class propertyBase
+{
+    public:
+        T dummy = T();
+        T& obj = dummy;
+
+        propertyBase(T& value) : obj(value) {}
+};
+
+// Read/Write specialization
+template <typename T, bool canWrite = true, bool isPod = true>
+class Property : private propertyBase<T>
+{
+public:
+    Property() = default;
+    
+    Property(const Property<T, true, true>& value): propertyBase<T>(value.obj) 
+    {}
+
+    Property(Property<T, true, true>& value): propertyBase<T>(value.obj)
+    {}
+
+    Property(T& value): propertyBase<T>(value)
+    {}
+
+    T& operator=(const Property<T, true, true>& value)
+    {
+        this->obj = value.obj;
+        return this->obj;
+    }
+
+    T& operator=(const T& value)
+    {
+        this->obj = value;
+        return this->obj;
+    }
+
+    T& operator=(T& value)
+    {
+        this->obj = value;
+        return this->obj;
+    }
+
+    T& get() const
+    {
+        return this->obj;
+    }
+
+    void set(const T& value)
+    {
+        this->obj = value;
+    }
+
+    operator T&() const
+    {
+        return this->get();
+    }    
+
+};
+
+// Read only specialization
+template <typename T>
+class Property<T, false, true> : private propertyBase<T>
+{
+public:
+    Property() = default;
+    
+    Property(const Property<T, false, true>& value): propertyBase<T>(value.obj) 
+    {}
+
+    Property(T& value): propertyBase<T>(value)
+    {}
+
+    T& operator=(const Property& value)
+    {
+        this->obj = value.obj;
+        return this->obj;
+    }
+
+    T get() const
+    {
+        return this->obj;
+    }
+
+    operator T() const
+    {
+        return this->get();
+    }    
+
+};
+
+template <typename T>
+class Property<T, true, false> : public T
+{
+public:
+    using T::T;
+
+    Property(const T& value) : T(value)
+    {}   
+
+    T* operator->()
+    {
+        return this;
+    } 
+};
+
+template <typename T>
+class Property<T, false, false> : public T
+{
+    const T* const _obj = this;
+public:
+    using T::T;
+
+    Property(const T& value) : T(value)
+    {} 
+
+    const T* const operator->() const
+    {
+        return this->_obj;
+    }
+};
+
+template <typename T>
+using roProperty = Property<T, false, std::is_pod<T>::value>;
+
+template <typename T>
+using rwProperty = Property<T, true, std::is_pod<T>::value>;
---
+++ example.cpp
@@ -0,0 +1,46 @@
+# include <iostream>
+# include <string>
+
+# include "Property.hpp"
+
+class Test
+{
+    int _var                = 1;
+    double _anothervar      = 10;
+    std::string _stringvar  = "what";
+    std::string _stringvar2 = "test";
+
+public:
+    
+    rwProperty<int> var                = _var; 
+    roProperty<double> anothervar      = _anothervar;
+    roProperty<std::string> stringvar  = _stringvar;
+    rwProperty<std::string> stringvar2 = _stringvar2;
+};
+
+int main()
+{
+    using std::cout;
+
+    Test test;
+
+    cout<< "Initial values:\n";
+    cout<< "var        = "<< test.var       <<"\n";
+    cout<< "anothervar = "<< test.anothervar<<"\n";
+    cout<< "strinvar   = "<< test.stringvar <<"\n";
+    cout<< "stringvar2 = "<< test.stringvar2<<"\n";
+
+    test.var += 5;
+
+    // Compile errors (read only properties)
+    // test.anothervar = 42;
+    // test.strinvar = "value";
+
+    test.stringvar2 = "value";
+
+    cout<< "\nNew values:\n";
+    cout<< "var        = "<< test.var       <<"\n";
+    cout<< "anothervar = "<< test.anothervar<<"\n";
+    cout<< "strinvar   = "<< test.stringvar <<"\n";
+    cout<< "stringvar2 = "<< test.stringvar2<<"\n";
+}
